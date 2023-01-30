d1beaf44c92520053496299d47a9e935368cb2ad
datchung95 <103572538+datchung95@users.noreply.github.com>
2023-01-30 14:45:20 +0700

    Add files via upload

--- src/Redux/Actions/GetAllData/GetAllDataAction.tsx
+++ src/Redux/Actions/GetAllData/GetAllDataAction.tsx
@@ -1,6 +1,5 @@
 import { collection, doc, getDoc, getDocs } from "firebase/firestore";
 import openNotificationWithIcon from "../../../Component/Nofitication/Notification";
-// import { DOMAIN } from "../../../Config/Domain/Domain";
 import { database } from "../../../configFirebase";
 
 export const getAllDataAction = (collectionParam: string, dispatchAction: any) => {
@@ -33,33 +32,3 @@ export const getDetailDataAction = (collectionParam: string, dispatchAction: any
         }
     }
 }
-
-// export const getAllDataAction = (collectionParam: string, dispatchAction: any) => {
-//     return async (dispatch: any) => {
-//         try {
-//             let arrData: any[] = [];
-//             await fetch(`${DOMAIN}/${collectionParam}`)
-//                 .then((response) => response.json())
-//                 .then((data) => data.documents.forEach((doc: any) => {
-//                     let indexLast = doc.name.lastIndexOf("/")
-//                     let idColection = doc.name.slice(indexLast + 1, doc.name.length - 1)
-//                     arrData.push({ ...doc.fields, id: idColection })
-//                 }));
-                
-//                 arrData.map((item: any) => {
-//                     for (const [key, value] of Object.entries(item)) {
-//                         if (typeof(value) === "object") {
-//                             let valueOfChild: object = value as object
-//                             for (const [childKey, childValue] of Object.entries(valueOfChild)) {
-//                                 item[key] = childValue
-//                             }
-//                         } 
-//                     }
-//                 })
-//             dispatch(dispatchAction(arrData))
-//         }
-//         catch (err) {
-
-//         }
-//     }
-// }
